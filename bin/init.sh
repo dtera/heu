9a5593453c3d9b7b4d855dfddf6b3dfb92997521
dterazhao <dterazhao@tencent.com>
2023-07-09 21:08:40 +0800

    [feat]: add all dependencies for each library

--- bin/init.sh
+++ bin/init.sh
@@ -37,13 +37,11 @@ if [ "$(uname)" == "Darwin" ]; then
     [ -e "$out_path" ] || find "$EXT_LIB_DIR"/"$m_path" -name "*.${LIB_SUFFIX}" | xargs libtool -"$LINK_T" -o "$out_path"
   done
 
-  out_path="$LIB_OUT_DIR"/libphe_all."${OUT_LIB_SUFFIX}"
-  [ -e "$out_path" ] && mv "$out_path" "$LIB_OUT_DIR"/libphe_all__."${OUT_LIB_SUFFIX}"
+  out_path="$LIB_OUT_DIR"/libphe."${OUT_LIB_SUFFIX}"
   # shellcheck disable=SC2038
-  find "$LIB_OUT_DIR" -name "*.${OUT_LIB_SUFFIX}" | xargs libtool -"$LINK_T" -o "$out_path"
+  [ -e "$out_path" ] || find "$LIB_OUT_DIR" -name "*.${OUT_LIB_SUFFIX}" | xargs libtool -"$LINK_T" -o "$out_path"
 elif [ "$(uname)" == "Linux" ]; then
   echo "Linux"
-  LIB_SUFFIX=so
   for module in algorithms phe numpy; do
     # shellcheck disable=SC2038
     find "$LIB_DIR/$module" -name "*_all.${LIB_SUFFIX}" | xargs -I{} cp {} "$LIB_OUT_DIR"
