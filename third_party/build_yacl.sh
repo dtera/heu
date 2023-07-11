ab6bffb4b72ed27758a4b73542bae2d0f8687b2a
dterazhao <dterazhao@tencent.com>
2023-07-11 19:26:07 +0800

    [feat]: remove include instead of generating automatically for cmake project

--- third_party/build_yacl.sh
+++ third_party/build_yacl.sh
@@ -16,8 +16,16 @@ else
   cp -dR "$CD"/$pkg/third_party/lib/* "$CD"/lib/
   cp -dR "$CD"/$pkg/third_party/lib64/* "$CD"/lib/
 fi
-cp -R "$CD"/$pkg/include/* "$CD"/include/
 cp -R "$CD"/$pkg/third_party/include/* "$CD"/include/
+#cp -R "$CD"/$pkg/include/* "$CD"/include/
+cd "$CD"/$pkg || exit 0
+# shellcheck disable=SC2044
+for path in $(find $pkg -name "*.h"); do
+  #head_file=${path##*/}
+  head_path=${path%/*}
+  head_save_path="$CD"/include/$head_path
+  mkdir -p "$head_save_path" && cp "$path" "$head_save_path"
+done
 rm -rf "$CD"/lib/cmake && rm -rf "$CD"/lib/pkgconfig
 cd "$CD"/$pkg && mkdir build && cd build || exit
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX="$CD/" ..
