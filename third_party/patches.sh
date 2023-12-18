a964f33b962315f5a3ee96478e7038fce5bf6db5
dterazhao <dterazhao@tencent.com>
2023-12-18 19:32:59 +0800

    [feat]: add patches.sh for cmake project

--- third_party/patches.sh
+++ third_party/patches.sh
@@ -5,4 +5,8 @@ cd "$CD" || exit
 
 f_path="include/yacl/math/mpint/mp_int.h"
 row_num=$(awk -v pattern="mp_int n_;" '$0 ~ pattern {print NR-1}' "$f_path")
-sed -i '' "${row_num}s/protected:/public:/" "$f_path"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "${row_num}s/protected:/public:/" "$f_path"
+else
+  sed -i "${row_num}s/protected:/public:/" "$f_path"
+fi
