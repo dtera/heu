6ab0cc56f30f1859ebbea7fe0e42a479f98c0ca7
dterazhao <dterazhao@tencent.com>
2023-12-22 17:09:34 +0800

    [feat]: update yacl for cmake project

---
+++ tarpkg.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CD=$(cd "$(dirname "$0")" || exit && pwd)
+cd "$CD" || exit
+
+pkg=heu
+mkdir -p tmp/$pkg
+cp -R include third_party $pkg CMakeLists.txt tmp/$pkg/
+# shellcheck disable=SC2164
+pushd tmp
+rm -rf $pkg/third_party/{include,lib,bazel_cpp,bazel_rust}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  gtar cvf $pkg.tar.gz $pkg
+else
+  tar cvf $pkg.tar.gz $pkg
+fi
+rm -f ../$pkg.tar.gz && mv $pkg.tar.gz ..
+# shellcheck disable=SC2164
+popd
+rm -rf tmp
