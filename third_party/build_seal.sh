3933cd4cd7398a927e51e1f989c711d4829ee37b
dterazhao <dterazhao@tencent.com>
2024-05-10 17:12:43 +0800

    feat: add seal for cmake project

---
+++ third_party/build_seal.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CD=$(cd "$(dirname "$0")" || exit && pwd)
+cd "$CD" || exit
+. "$CD"/versions.sh
+
+# build seal
+# [ -d benchmark ] || git clone https://github.com/microsoft/SEAL.git
+# shellcheck disable=SC2154
+pkg=seal-"$seal_ver"
+download_url=https://github.com/microsoft/SEAL/archive/refs/tags/v"$seal_ver".tar.gz
+sh "$CD"/build_template.sh --pkg "$pkg" -u "$download_url" -o \
+"-DSEAL_USE_MSGSL=OFF -DSEAL_BUILD_DEPS=OFF -DSEAL_USE_ZSTD=OFF -DSEAL_USE_ZLIB=OFF -DSEAL_THROW_ON_TRANSPARENT_CIPHERTEXT=OFF"
+
+m_ver=${seal_ver%.*}
+rm -rf "$CD"/include/seal && mv "$CD"/include/SEAL-$m_ver/seal "$CD"/include/ && rm -rf "$CD"/include/SEAL-$m_ver
--- third_party/versions.sh
+++ third_party/versions.sh
@@ -6,4 +6,6 @@ cd "$CD" || exit
 # shellcheck disable=SC2034
 gmp_ver=6.3.0
 # shellcheck disable=SC2034
+seal_ver=4.1.1
+# shellcheck disable=SC2034
 cgbn_ver=e8b9d265c7b84077d02340b0986f3c91b2eb02fb
